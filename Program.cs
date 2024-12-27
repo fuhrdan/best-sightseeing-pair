c54e8f5353f2f32abaa55251baebf50bded1e982
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-27 06:11:58 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//*****************************************************************************
+//** 1014. Best Sightseeing Pair                                    leetcode **
+//*****************************************************************************
+
+
+int maxScoreSightseeingPair(int* values, int valuesSize) {
+    int ans = 0;
+    int mx = values[0];
+    
+    for (int j = 1; j < valuesSize; ++j)
+    {
+        if (ans < values[j] - j + mx)
+        {
+            ans = values[j] - j + mx;
+        }
+        
+        if (mx < values[j] + j)
+        {
+            mx = values[j] + j;
+        }
+    }
+    
+    return ans;
+}
